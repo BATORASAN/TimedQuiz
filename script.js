34e22b46504f2a6f143e2d54f71248ec61cf8fc1
Tora <waltva0@gmail.com>
2021-08-21 21:47:58 -0500

    Added timer functionality, fixed coding issues, made elements appear only at game start.

--- script.js
+++ script.js
@@ -5,37 +5,41 @@ var myScore = 0;
 var questionNumber = 0;
 var questionAnswer = "";
 
+var timerCount=60;
+
 // Array of questions, compares chosen answer to correct answer.
 var questionArray = [
-  {
-    question: "What is the capital of New York State?",
-    choiceA: "Albany",
-    choiceB: "Buffalo",
-    choiceC: "New York City",
-    correctChoice: "ChoiceA",
-  },
-  {
-    question: "Which of these months have 30 days total?",
-    choiceA: "October",
-    choiceB: "April",
-    choiceC: "May",
-    correctChoice: "ChoiceB",
-  },
-  {
-    question: "Which of these internet browsers has been discontinued?",
-    choiceA: "Opera",
-    choiceB: "Mozilla Firefox",
-    choiceC: "Internet Explorer",
-    correctChoice: "ChoiceC",
-  },
-  {
-    question: "What is the 5th planet from the sun?",
-    choiceA: "Jupiter",
-    choiceB: "Mars",
-    choiceC: "Saturn",
-    correctChoice: "ChoiceA",
-  },
+    {
+        question: "What is the capital of New York State?",
+        choiceA: "Albany",
+        choiceB: "Buffalo",
+        choiceC: "New York City",
+        correctChoice: "ChoiceA",
+    },
+    {
+        question: "Which of these months have 30 days total?",
+        choiceA: "October",
+        choiceB: "April",
+        choiceC: "May",
+        correctChoice: "ChoiceB",
+    },
+    {
+        question: "Which of these internet browsers has been discontinued?",
+        choiceA: "Opera",
+        choiceB: "Mozilla Firefox",
+        choiceC: "Internet Explorer",
+        correctChoice: "ChoiceC",
+    },
+    {
+        question: "What is the 5th planet from the sun?",
+        choiceA: "Jupiter",
+        choiceB: "Mars",
+        choiceC: "Saturn",
+        correctChoice: "ChoiceA",
+    },
 ];
+var start = `<button id="Start">Start Game!</button>`;
+document.getElementById("start").innerHTML = start;
 
 var question = `<div id="Question">${questionArray[questionNumber].question}</div>
 <button id="ChoiceA">${questionArray[questionNumber].choiceA}</button>
@@ -43,54 +47,65 @@ var question = `<div id="Question">${questionArray[questionNumber].question}</di
 <button id="ChoiceC">${questionArray[questionNumber].choiceC}</button>`;
 document.getElementById("quiz").innerHTML = question;
 
-var timerCount = 60;
+document.getElementById("Question").style.display = "none";
+document.getElementById("ChoiceA").style.display = "none";
+document.getElementById("ChoiceB").style.display = "none";
+document.getElementById("ChoiceC").style.display = "none";
 
-// startTimer();
+var onAnswer = function (event) {
+    console.log(event);
+    var questionAnswer = event.target.id;
+    console.log(questionAnswer, questionArray[questionNumber].correctChoice);
+    if (questionAnswer === questionArray[questionNumber].correctChoice) {
+        console.log("You got it right");
+    } else {
+        timerCount -= 5;
+        console.log("You got it wrong", timerCount);
+    }
+    questionNumber++;
+    console.log(questionNumber);
 
-document.getElementById("ChoiceA").addEventListener("click", function (event) {
+    document.getElementById("Question").innerText = questionArray[questionNumber].question;
+    document.getElementById("ChoiceA").innerText = questionArray[questionNumber].choiceA;
+    document.getElementById("ChoiceB").innerText = questionArray[questionNumber].choiceB;
+    document.getElementById("ChoiceC").innerText = questionArray[questionNumber].choiceC;
+};
+
+var startGame = function (event) {
   console.log(event);
-  var questionAnswer = event.target.id;
-  console.log(questionAnswer, questionArray[questionNumber].correctChoice);
-  if (questionAnswer === questionArray[questionNumber].correctChoice) {
-    console.log("You got it right");
-  } else {
-    timerCount -= 5;
-    console.log("You got it wrong", timerCount);
+  document.getElementById("Question").style.display = "block";
+  document.getElementById("ChoiceA").style.display = "block";
+  document.getElementById("ChoiceB").style.display = "block";
+  document.getElementById("ChoiceC").style.display = "block";
+  document.getElementById("Start").style.display = "none";
+
+var counter=setInterval(timer, 1000); //1000 will  run it every 1 second
+
+function timer()
+{
+  timerCount=timerCount-1;
+  if (timerCount <= 0)
+  {
+     clearInterval(counter);
+     endGame()
+     return;
   }
-  questionNumber++;
-  console.log(questionNumber);
-});
-document.getElementById("ChoiceB").addEventListener("click", function (event) {
-  console.log(event);
-  var questionAnswer = event.target.id;
-  console.log(questionAnswer, questionArray[questionNumber].correctChoice);
-  if (questionAnswer === questionArray[questionNumber].correctChoice) {
-    console.log("You got it right");
-  } else {
-    timerCount -= 5;
-    console.log("You got it wrong", timerCount);
-  }
-  questionNumber++;
-  console.log(questionNumber)
-});
-document.getElementById("ChoiceC").addEventListener("click", function (event) {
-  console.log(event);
-  var questionAnswer = event.target.id;
-  console.log(questionAnswer, questionArray[questionNumber].correctChoice);
-  if (questionAnswer === questionArray[questionNumber].correctChoice) {
-    console.log("You got it right");
-  } else {
-    timerCount -= 5;
-    console.log("You got it wrong", timerCount);
-  }
-  questionNumber++;
-  console.log(questionNumber)
-});
 
-// nextQuestion();
+  document.getElementById("timer").innerHTML=timerCount + " seconds"; 
+}
+}
 
-// endGame();
+function endGame() {
+document.getElementById("Question").style.display = "none";
+document.getElementById("ChoiceA").style.display = "none";
+document.getElementById("ChoiceB").style.display = "none";
+document.getElementById("ChoiceC").style.display = "none";
+document.getElementById("Start").style.display = "none";
+document.getElementById("Timer").style.display = "none";
+}
 
-// displayScore();
+document.getElementById("Start").addEventListener("click", startGame);
+document.getElementById("ChoiceA").addEventListener("click", onAnswer);
+document.getElementById("ChoiceB").addEventListener("click", onAnswer);
+document.getElementById("ChoiceC").addEventListener("click", onAnswer);
 
-// // Local storage
